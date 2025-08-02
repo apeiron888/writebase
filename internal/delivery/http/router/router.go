d1ddcaf47a8326c3d61e32fe2bc9666b5faf539d
apeiron888 <aelafgetaneh@gmail.com>
2025-08-02 23:17:37 +0300

    article controller definition

--- internal/delivery/http/router/router.go
+++ internal/delivery/http/router/router.go
@@ -1,1 +1,8 @@
 package router
+
+// Consistent parameter naming:
+// GET /articles/:id          // UUID
+// GET /view/:slug            // SEO-friendly
+// GET /tags/:tag             // URL-encoded
+// POST /:id/claps            // Engagement action
+
